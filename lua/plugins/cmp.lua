3305bbc351afc80b224950fa70443f30318a8966
Makoto Uwaya <makoto.uwaya@e-seikatsu.co.jp>
2025-12-24 12:56:31 +0900

    ➕ Add plugin nvim-cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,60 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  -- lazy.nvim はデフォルトで plugin名(nvim-cmp)から setup を探そうとしますが、
+  -- このプラグインのメインモジュールは "cmp" なので指定しておくと確実です。
+  main = "cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+  },
+  -- opts を関数にすることで、設定が必要になった瞬間までモジュールのロードを遅延できます
+  opts = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    return {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    }
+  end,
+}
+
